d7b1ec6011b4db2ef111dafa7b4fb72d0830c9e6
PetrSergeevich3301 <petros00715@gmail.com>
2023-07-20 14:25:06 +0300

    1

--- Peter`sToDoManager/Pages/CRUD/CompleteTask.cshtml.cs
+++ Peter`sToDoManager/Pages/CRUD/CompleteTask.cshtml.cs
@@ -5,8 +5,20 @@ namespace Peter_sToDoManager.Pages.CRUD
 {
     public class CompleteTaskModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDbContext _context;
+        public CompleteTaskModel(AppDbContext context)
         {
+            _context = context;
+        }
+        public ActionResult OnGet(int id)
+        {
+            var task = _context.Tasks.FirstOrDefault(t => t.Id == id);
+            if (task == null) {
+                return RedirectToPage("/ViewAllTasks");
+            }
+            task.Complete= true;
+            _context.SaveChanges();
+            return RedirectToPage("/ViewAllTasks");
         }
     }
 }
